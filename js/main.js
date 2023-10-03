bb5bdcdef9209c9c975ba5a71ea58a272075a4b1
rtmoffat <rtmoffat@gmail.com>
2023-10-03 19:40:02 -0400

    Small updates

--- js/main.js
+++ js/main.js
@@ -1,158 +1,95 @@
-//Auto-refactored code from ChatGPT from original Python script
+function initGui() {
+  $( function() {
+    $( ".draggable" ).draggable({revert:"invalid",revertDuration:200,opacity: 0.35});
+    $( ".droppable" ).droppable({
+        tolerance:"pointer",
+        classes: {
+            "ui-droppable-hover": "highlightTarget"
+            },
+        drop: function( event, ui ) {
+            //alert('dropped!');
+            $("#thwack")[0].play();
+            ui.draggable.remove();
+        }
+      });
+    });
+}
+
 const fs = require('fs');
 const readline = require('readline');
 
+let enemies;
+enemies=JSON.parse(fs.readFileSync('../data/enemies.json','utf-8'))
+console.log(enemies)
+
 let cards;
+cards=JSON.parse(fs.readFileSync('../data/cards.json', 'utf8'))
+console.log(cards)
 
-fs.readFile('../data/cards.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading cards.json:', err);
-    return;
+class Deck {
+  constructor(cards) {
+    this.drawPile = [];
+    this.discardPile = [];
+    for (const card of cards) {
+      this.drawPile.push(card);
+    }
   }
-  cards = JSON.parse(data);
 
-  let enemies;
-
-fs.readFile('../data/enemies.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading enemies.json:', err);
-    return;
+  draw() {
+    return this.drawPile.pop();
   }
-  enemies = JSON.parse(data);
 
-  class Deck {
-    constructor(cards) {
-      this.drawPile = [];
+  discard(card) {
+    this.discardPile.push(card);
+  }
+
+  shuffle(includeDiscardPile) {
+    if (includeDiscardPile) {
+      this.drawPile = this.drawPile.concat(this.discardPile);
       this.discardPile = [];
-      for (const card of cards) {
-        this.drawPile.push(card);
-      }
     }
-
-    draw() {
-      return this.drawPile.pop();
-    }
-
-    discard(card) {
-      this.discardPile.push(card);
-    }
-
-    shuffle(includeDiscardPile) {
-      if (includeDiscardPile) {
-        this.drawPile = this.drawPile.concat(this.discardPile);
-        this.discardPile = [];
-      }
-      for (let i = this.drawPile.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.drawPile[i], this.drawPile[j]] = [this.drawPile[j], this.drawPile[i]];
-      }
+    for (let i = this.drawPile.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.drawPile[i], this.drawPile[j]] = [this.drawPile[j], this.drawPile[i]];
     }
   }
+}
 
-  class Player {
-    constructor(hand, health, mana, drawLimit = 6, handSizeLimit = 6) {
-      this.hand = hand;
-      this.health = health;
-      this.mana = mana;
-      this.drawLimit = drawLimit;
-      this.handSizeLimit = handSizeLimit;
-    }
-
-    resetMana() {
-      this.mana = 6;
-    }
+class Player {
+  constructor(hand, health, mana, drawLimit = 6, handSizeLimit = 6) {
+    this.hand = hand;
+    this.health = health;
+    this.mana = mana;
+    this.drawLimit = drawLimit;
+    this.handSizeLimit = handSizeLimit;
   }
 
-  class Enemy {
-    constructor(name, health, dmg) {
-      this.name = name;
-      this.health = health;
-      this.dmg = dmg;
-    }
+  resetMana() {
+    this.mana = 6;
   }
+}
 
-  class Card {
-    constructor(name, type, cost, dmg) {
-      this.name = name;
-      this.type = type;
-      this.cost = cost;
-      this.dmg = dmg;
-    }
+class Enemy {
+  constructor(name, health, dmg) {
+    this.name = name;
+    this.health = health;
+    this.dmg = dmg;
   }
+}
 
-  const pDeck = new Deck(cards);
-  console.log(pDeck.drawPile);
-  pDeck.shuffle();
-  console.log(pDeck.drawPile);
-
-  // Init player
-  const player1 = new Player([], 90, 6);
-
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  function pDraw() {
-    // Draw up to draw limit
-    for (let i = 0; i < player1.drawLimit; i++) {
-      if (player1.hand.length < player1.drawLimit) {
-        // Reshuffle discard pile if draw pile is empty
-        if (pDeck.drawPile.length === 0) {
-          pDeck.shuffle(true);
-        }
-        player1.hand.push(pDeck.draw());
-      }
-    }
+class Card {
+  constructor(name, type, cost, dmg) {
+    this.name = name;
+    this.type = type;
+    this.cost = cost;
+    this.dmg = dmg;
   }
+}
 
-  async function main() {
-    const troll = new Enemy('Leroy', 50, 10);
-
-    while (troll.health > 0) {
-      // Draw cards up to hand limit
-      pDraw();
-      const play = await askQuestion('Play a card? (Y/N) ');
-
-      if (play.toUpperCase() === 'Y') {
-        // Display player hand
-        for (let cardIndex = 0; cardIndex < player1.hand.length; cardIndex++) {
-          const card = player1.hand[cardIndex];
-          console.log(`${cardIndex} ${card.Name} ${card.Cost}`);
-        }
-        const choice = await askQuestion('Choose a card to play: ');
-        if (player1.hand[choice].Cost <= player1.mana) {
-          troll.health -= player1.hand[choice].Damage;
-          player1.mana -= player1.hand[choice].Cost;
-          pDeck.discard(player1.hand[choice]);
-          player1.hand.splice(choice, 1);
-        } else {
-          console.log('Insufficient mana!');
-        }
-      } else {
-        player1.resetMana();
-        player1.health -= troll.dmg;
-      }
-
-      console.log(`Player: ${player1.health}`);
-      console.log(`Enemy: ${troll.health}`);
-      console.log(`Player Mana: ${player1.mana}`);
-    }
-
-    if (troll.health <= 0) {
-      console.log('You beat the troll!!');
-    } else {
-      console.log('Ran out of ammo!');
-    }
-
-    rl.close();
-  }
-
-  function askQuestion(question) {
-    return new Promise((resolve) => {
-      rl.question(question, resolve);
-    });
-  }
-
-  main();
-});
+const pDeck = new Deck(cards);
+console.log(pDeck.drawPile);
+pDeck.shuffle();
+console.log(pDeck.drawPile);
+const player1 = new Player([], 90, 6);
+console.log(player1.health)
+initGui();
