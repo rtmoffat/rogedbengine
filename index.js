ed5d12eab8f6c662b50a4fcc7eca614bfb570ea4
rtmoffat <rtmoffat@gmail.com>
2023-10-09 11:12:59 -0400

    Refactored data to Node

--- index.js
+++ index.js
@@ -13,9 +13,43 @@ app.get('/', (req, res) => {
     }
   })
 
-/*app.get('/', function (req, res) {
-  res.send('hello world')
-})*/
+function loadData() {
+  var data={};
+  //const location=window.location.hostname;
+  const location="";
+  if (location=='localhost' || location=='127.0.0.1') {
+    console.log('Loading locally');
+    cards=getLocalCards();
+    enemies=getLocalEnemies();
+    levels=getLocalLevels();
+    console.log(cards);
+    console.log(enemies);
+    console.log(levels);
+    data['cards']=cards;
+    data['enemies']=enemies;
+    data['levels']=levels;
+  }
+  else {
+    console.log('Loading remotely');
+    const fs = require('fs');
+    const readline = require('readline');
+    
+    enemies=JSON.parse(fs.readFileSync('./data/enemies.json','utf-8'));
+    console.log(enemies);
+    
+    cards=JSON.parse(fs.readFileSync('./data/cards.json', 'utf8'));
+    console.log(cards);
+    
+    levels=JSON.parse(fs.readFileSync('./data/levels.json', 'utf8'));
+    console.log(levels);
+    data['cards']=cards;
+    data['enemies']=enemies;
+    data['levels']=levels;
+  }
+  return data;
+}
+var data={};
+data=loadData();
 
-
-app.listen(3000)
+app.listen(3000);
+console.log("listening");
