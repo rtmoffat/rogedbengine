a226e50d1feef94986d88caac720e0766ec61daa
rtmoffat <rtmoffat@gmail.com>
2023-10-16 14:14:08 -0400

    ssl for endpoints

--- endpoint_template.js
+++ endpoint_template.js
@@ -1,3 +1,5 @@
+const https=require('https');
+const fs = require('fs');
 var express = require('express')
 var app = express()
 const path=require('path')
@@ -24,7 +26,7 @@ function loadData() {
   }
   else {
     console.log('Loading remotely');
-    const fs = require('fs');
+    //const fs = require('fs');
     const readline = require('readline');
     
     enemies=JSON.parse(fs.readFileSync('./data/enemies.json','utf-8'));
@@ -114,5 +116,15 @@ app.get('/state',(req,res) => {
   res.json(data);
 });
 
-app.listen(3000);
+https
+  .createServer(
+    {
+      key:fs.readFileSync("/certs/schplorph.com.key"),
+      cert:fs.readFileSync("/certs/schplorph.com.cert")
+    },
+    app)
+  .listen(3000,() => {
+    console.log('server running on port 3000')
+  });
+//app.listen(3000);
 console.log("listening");
